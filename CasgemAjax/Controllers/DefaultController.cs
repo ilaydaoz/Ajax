816df8a7c8c5feba1c738b04e65f714c79d6e448
ilayda <ilaydaozken@gmail.com>
2023-07-31 12:18:17 +0300

    UpdateCustomer,GetCustomer

--- CasgemAjax/Controllers/DefaultController.cs
+++ CasgemAjax/Controllers/DefaultController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CasgemAjax.Controllers
 {
@@ -27,5 +28,26 @@ namespace CasgemAjax.Controllers
             var values = JsonConvert.SerializeObject(costumer);
             return Json(values);
         }
+
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = context.Costumers.Find(id);
+            context.Costumers.Remove(value);
+            context.SaveChanges();
+            return NoContent();
+        }
+        public IActionResult GetCustomer(int CostumerID)
+        {
+            var value = context.Costumers.Find(CostumerID);
+           var jsonCustomer = JsonConvert.SerializeObject(value);
+            return Json(jsonCustomer);
+        }
+        public IActionResult UpdateCustomer(Costumer costumer)
+        {
+           context.Costumers.Update(costumer);
+            context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
